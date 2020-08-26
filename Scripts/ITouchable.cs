b4e432f7a13c274c8977238e64bbae1704d4cb39
Lexscite <lexscite@gmail.com>
2020-08-26 15:54:35 +0300

    Initial

---
+++ Scripts/ITouchable.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace PaperStag
+{
+public interface ITouchable { }
+
+public interface ITouchableOnBegan : ITouchable
+{
+    [PublicAPI]
+    void OnTouchBegan(Vector2 position);
+}
+
+public interface ITouchableOnEnded : ITouchable
+{
+    [PublicAPI]
+    void OnTouchEnded(Vector2 position);
+}
+}
